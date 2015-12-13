0616246970ce06fef727311389f46854ff1fc839
Tobias Müller <Tobias_Mueller@twam.info>
2015-12-13 09:17:55 +0100

    Added UUID as database keys. Added add/update/delete methods to database

--- Database.py
+++ Database.py
@@ -1,6 +1,8 @@
 import PersistentDict
 import collections
 import copy
+import uuid
+import time
 
 
 class Database:
@@ -11,8 +13,8 @@ class Database:
         'quantity': {'type': int, 'default': 0},
         'distributor': {'type': dict, 'default': {}},
         'datasheetURL': {'type': str, 'default': ''},
-        'timestampCreated': {'type': int, 'default': 0},
-        'timestampLastModified': {'type': int, 'default': 0},
+        'timestampCreated': {'type': float, 'default': 0.0},
+        'timestampLastModified': {'type': float, 'default': 0.0},
     }
 
     def __init__(self, filename):
@@ -36,3 +38,36 @@ class Database:
         filteredDict = {k: self.addDefaults(copy.copy(v)) if addDefaults else copy.copy(
             v) for k, v in self.persistentDict.items() if (filter(k) if filter != None else True)}
         return collections.OrderedDict(sorted(filteredDict.items(), key=lambda t: t[1][orderBy]))
+
+    def add(self, val):
+        # create key for part and make sure that is not already preset
+        partKey = None
+        while (partKey == None) or (partKey in self.persistentDict):
+            partKey = str(uuid.uuid4())
+
+        partData = copy.copy(val)
+
+        # add timestamps
+        partData['timestampCreated'] = time.time()
+        partData['timestampLastModified'] = time.time()
+
+        self.persistentDict[partKey] = partData
+
+    def update(self, partKey, val):
+        # check if key is existent
+        if partKey not in self.persistentDict:
+            raise Exception('Key \'%s\' not in database.' % (partKey))
+
+        partData = copy.copy(val)
+
+        # update timestamps
+        partData['timestampLastModified'] = time.time()
+
+        self.persistentDict[partKey] = partData
+
+    def delete(self, partKey):
+        # check if key is existent
+        if partKey not in self.persistentDict:
+            raise Exception('Key \'%s\' not in database.' % (partKey))
+
+        del self.persistentDict[partKey]
