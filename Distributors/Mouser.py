467bb1d889d675d46aacbcf8782d25f60efffea5
Tobias Müller <Tobias_Mueller@twam.info>
2016-05-22 18:51:44 +0200

    Changed Mouser to SOAP Search API

--- Distributors/Mouser.py
+++ Distributors/Mouser.py
@@ -5,9 +5,13 @@ import urllib.request
 import bs4
 import Database
 import urllib.parse
-
+import suds.client
+import logging
+import config
 
 class Mouser(__Distributor.Distributor):
+    WSDL_URL = "http://www.mouser.de/service/searchapi.asmx?WSDL"
+
     GROUP_MAP = {
         'K': None,  # Customer PO
         '14K': None,  # Line Item
@@ -64,78 +68,68 @@ class Mouser(__Distributor.Distributor):
         else:
             return None
 
+    def meta_redirect(self, content):
+        soup  = bs4.BeautifulSoup(content, 'html5lib')
+
+        result = soup.find("meta",attrs={"http-equiv":"Refresh"})
+        if result:
+            wait,text=result["content"].split(";")
+            if text.lower().startswith("url="):
+                url=text[4:]
+                return url
+        return None
+
     def getData(self, data):
         newData = None
 
-        if 'distributorPartNumber' in data['distributor'][self.name()]:
-            url = "http://www.mouser.com/Search/Refine.aspx?Keyword={}".format(
-                data['distributor'][self.name()]['distributorPartNumber'])
-        else:
+        if 'distributorPartNumber' not in data['distributor'][self.name()]:
             raise Exception('No valid key found to query for data!')
 
-        if self.partDB.args.debug:
-            print('Loading URL %s ...' % url)
+        distributorPartNumber = data['distributor'][self.name()]['distributorPartNumber']
 
-        req = urllib.request.Request(
-            url, headers={'User-Agent': "electronic-parser"})
-        page = urllib.request.urlopen(req)
-        soup = bs4.BeautifulSoup(page.read(), 'html5lib')
+        soapClient = suds.client.Client(self.WSDL_URL)
+        mouserHeader = soapClient.factory.create('MouserHeader')
+        mouserHeader.AccountInfo.PartnerID = config.MOUSER_API_KEY
+        soapClient.set_options(soapheaders=mouserHeader)
+        result = soapClient.service.SearchByPartNumber(distributorPartNumber)
 
-        searchResults = soup.find_all('div', id='searchResultsTbl')
-        if searchResults != []:
-            if self.partDB.args.debug:
-                print('Found search results on paging. Trying to find correct part.')
+        print(result)
 
-            for row in searchResults[0].find_all(
-                    'tr', class_=lambda x: x == "SearchResultsRowOdd" or x == "SearchResultsRowEven"):
+        if result.NumberOfResult == 0:
+            raise Exception('Part number not found.')
 
-                if row['data-partnumber'] == data['distributor'][self.name()
-                                                                 ]['distributorPartNumber']:
+        partId = 0
+        while True:
+            if result.Parts.MouserPart[0].MouserPartNumber == distributorPartNumber:
+                break
 
-                    cols = row.find_all('td')
-                    link_col = cols[2]
-                    newRelUrl = link_col.find_all('a')[0]['href']
-                    newUrl = urllib.parse.urljoin(url, newRelUrl)
+            partId+=1
 
-                    if self.partDB.args.debug:
-                        print('Part found! Loading new URL %s ...' % newUrl)
+            if partId > result.NumberOfResult-1:
+                raise Exception('Part number not found in results.')
 
-                    req = urllib.request.Request(
-                        newUrl, headers={'User-Agent': "electronic-parser"})
-                    page = urllib.request.urlopen(req)
-                    soup = bs4.BeautifulSoup(page.read(), 'html5lib')
 
-                    break
-
-            if newUrl is None:
-                raise Exception("Did not find part in search results")
+        print(result.Parts.MouserPart[0].Description)
 
         # basic data
-        productDesc = soup.find_all('div', id='product-desc')
         newData = {
             "distributor": {
                 "mouser": {
                     "distributorName": "mouser",
-                    "distributorPartNumber": productDesc[0].find_all('div', itemprop="model")[0].get_text().strip(),
+                    "distributorPartNumber": distributorPartNumber,
                 }
             },
-            "manufacturerPartNumber": productDesc[0].find_all('div', itemprop="ProductID")[0].get_text().strip(),
-            "description": productDesc[0].find_all('span', itemprop="description")[0].get_text().strip()
+            "manufacturerPartNumber": result.Parts.MouserPart[0].MouserPartNumber,
+            "description": result.Parts.MouserPart[0].Description
         }
 
         # try to get some additional specs
-        specs = soup.find_all('div', id='specs')
 
-        for row in specs[0].find_all(
-                'tr', class_=lambda x: x == "odd" or x == "even"):
-            cells = row.find_all("td")
-            key = cells[0].get_text().strip()
-            val = cells[1].get_text().strip()
+        newData['manufacturerName'] = result.Parts.MouserPart[0].Manufacturer
+        newData['datasheetURL'] = result.Parts.MouserPart[0].DataSheetUrl
 
-            if key == 'Manufacturer:':
-                newData['manufacturerName'] = val
-            elif key == 'Package/Case:':
-                newData['footprint'] = val
+        if 'Package' in result.Parts.MouserPart[0]:
+            newData['footprint'] = result.Parts.MouserPart[0].Package
 
         data = copy.copy(data)
         Database.mergeData(data, newData)
